fd31ebbae297ca8eeb5294b3c6ad4ba11de6a4bd
kyungbinYoo <ykb0314y@naver.com>
2024-08-12 11:15:39 +0900

    first_commint

---
+++ ktml/src/main/java/com/springboot/ktml/KtmlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ktml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtmlApplication.class, args);
+    }
+
+}
---
+++ ktml/src/test/java/com/springboot/ktml/KtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ktml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
